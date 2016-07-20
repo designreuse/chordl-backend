884da176efa0b2c1a8bbfcd2afd5e7cba97f0c1f
zak <zak@swingpulse.com>
2016-07-20 17:32:23 +0300

    init spring boot

---
+++ chords-api-rest/src/main/java/com/robotnec/chords/ChordsApplication.java
@@ -0,0 +1,15 @@
+package com.robotnec.chords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Kyrylo Zapylaiev zapylaev@gmail.com
+ */
+@SpringBootApplication
+public class ChordsApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ChordsApplication.class, args);
+    }
+}
