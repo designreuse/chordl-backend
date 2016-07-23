f5a6221bd000d33d682286de0366ab919e68f5f1
zak <zak@swingpulse.com>
2016-07-23 14:00:23 +0300

    add swagger

--- chords-api-rest/src/main/java/com/robotnec/chords/ChordsApplication.java
+++ chords-api-rest/src/main/java/com/robotnec/chords/ChordsApplication.java
@@ -1,5 +1,6 @@
 package com.robotnec.chords;
 
+import com.robotnec.chords.config.SwaggerConfig;
 import com.robotnec.chords.persistence.PersistenceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Import;
  * @author Kyrylo Zapylaiev zapylaev@gmail.com
  */
 @SpringBootApplication
-@Import(PersistenceConfiguration.class)
+@Import({PersistenceConfiguration.class, SwaggerConfig.class})
 public class ChordsApplication {
 
     public static void main(String[] args) throws Exception {
