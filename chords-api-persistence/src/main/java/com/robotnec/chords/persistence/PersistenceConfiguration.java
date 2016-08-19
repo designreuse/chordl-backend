d82636619d839fa8ef77439a747ca01a433115d7
zak <zak@robotnec.com>
2016-08-19 16:59:40 +0300

    Minor changes, change search highlighting

--- chords-api-persistence/src/main/java/com/robotnec/chords/persistence/PersistenceConfiguration.java
+++ chords-api-persistence/src/main/java/com/robotnec/chords/persistence/PersistenceConfiguration.java
@@ -1,14 +1,11 @@
 package com.robotnec.chords.persistence;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-import org.apache.solr.client.solrj.SolrServer;
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 
 import javax.sql.DataSource;
 
