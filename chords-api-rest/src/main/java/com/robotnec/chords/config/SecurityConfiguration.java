d1e76392bf47db1d07aa5db0be7b3acd9bdf83d2
zak <zak@swingpulse.com>
2016-08-28 19:53:26 +0300

    Fixed logout

--- chords-api-rest/src/main/java/com/robotnec/chords/config/SecurityConfiguration.java
+++ chords-api-rest/src/main/java/com/robotnec/chords/config/SecurityConfiguration.java
@@ -16,19 +16,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/", "welcome")
-                .access("hasRole('ROLE_USER')")
-                .and()
+                    .antMatchers("/resources/**", "/registration").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
                 .formLogin()
                     .loginPage("/login")
-                    .defaultSuccessUrl("/welcome")
-                    .failureUrl("/login?error")
-                    .usernameParameter("username")
-                    .passwordParameter("password")
-                .and()
-                .logout().logoutSuccessUrl("/login?logout");
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
     }
 
     @Autowired
