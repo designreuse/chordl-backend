10bda992a7becec9d28758f58b35e5c4b7f12d1e
zak <zak@swingpulse.com>
2016-08-28 16:04:24 +0300

    Remove fb

--- chords-api-rest/src/main/java/com/robotnec/chords/config/SecurityConfiguration.java
+++ chords-api-rest/src/main/java/com/robotnec/chords/config/SecurityConfiguration.java
@@ -1,11 +1,9 @@
 package com.robotnec.chords.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@EnableOAuth2Sso
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -13,13 +11,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST,  "/login/**")
-                .permitAll()
-                .antMatchers(HttpMethod.OPTIONS, "/login/**")
-                .permitAll()
                 .anyRequest()
-                .authenticated();
+                .anonymous();
     }
 }
